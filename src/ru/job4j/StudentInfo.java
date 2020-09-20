a285aa284cac61d745b7ddfe924f3bed3816927c
WestKort <westkort@gmail.com>
2020-09-20 21:09:35 +0300

    3.3. Разбор первой программы. [#367201]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String[] arg) {
+        System.out.println("Ник Чернов");
+        System.out.println("01.05.1988");
+    }
+}
