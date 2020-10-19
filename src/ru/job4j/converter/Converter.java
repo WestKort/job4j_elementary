81ee07c932645500d1d20f7e4ceec3db9fc4fcd0
WestKort <westkort@gmail.com>
2020-10-19 23:24:19 +0300

    0. Что такое тестирование [#367213]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -5,11 +5,14 @@ public class Converter {
         int rsl = value / 70;
         return rsl;
     }
+
     public static int rubleToDollar(int value) {
-        int rsl = value/60; /* формула перевода рублей в доллары. */
+        int rsl = value / 60; /* формула перевода рублей в доллары. */
         return rsl;
     }
+
     public static void main(String[] args) {
+
         int euro = Converter.rubleToEuro(140);
         int dollar = Converter.rubleToDollar(360);
         System.out.println("140 rubles are " + euro + " euro.");
