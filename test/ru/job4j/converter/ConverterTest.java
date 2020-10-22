1a52f2f3bfc982c75509c63b99a057fd28445c92
WestKort <westkort@gmail.com>
2020-10-22 23:21:09 +0300

    1. Модульные тесты. [#367214]

--- test/ru/job4j/converter/ConverterTest.java
+++ test/ru/job4j/converter/ConverterTest.java
@@ -7,9 +7,17 @@ public class ConverterTest {
 
     @Test
     public void whenConvert180RblThen3Dlr() {
-        int in = 180;
-        int expected = 3;
+        int in = 240;
+        int expected = 4;
         int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
         Assert.assertEquals(expected, out);
     }
 }
