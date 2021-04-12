02e619d9868bb35b7c5cb439bff0930f50cc8d90
WestKort <westkort@gmail.com>
2021-04-13 00:38:32 +0300

    4.1. Операторы ветвлений [#367144]

--- src/ru/job4j/condition/AlertDivByZero.java
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -6,6 +6,7 @@ public class AlertDivByZero {
         possibleDiv(-2);
     }
 
+
     public static void possibleDiv(int number) {
         if (number == 0) {
             System.out.println("Could not div by 0.");
