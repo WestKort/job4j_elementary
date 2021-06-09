b4d4be6baca66ec995e5faed406f4f751d25754b
WestKort <westkort@gmail.com>
2021-06-09 23:43:06 +0300

    4.2. Тернарное сравнение [#189 #136056]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,26 +2,11 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left;
-        boolean condition = left == 1;
-        int result1 = condition ? left : right;
+        int result = left > right ? left : right;
         return result;
     }
     public static void main(String[] args) {
-        String cond = Max.max(1, 2);
+        int cond = Max.max(3, 2);
         System.out.println(cond);
     }
 }
-
-
-   // public static String permission(int age) {
-     //   String label = age >= 18 ? "Please, enjoy it." : "You can't watch it.";
-       // return label;
-   //boolean can = age >= 18;
-  //  String label = can ? "Please, enjoy it." : "You can't watch it.";
-//return label;
-
-
-   // public static void main(String[] args) {
-      //  String msg = Film.permission(33);
-    //    System.out.println(msg);
